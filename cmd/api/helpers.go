e169a60e49e63e305d393b21843881e3092a2419
🐢 <tpham2580@gmail.com>
2024-12-28 14:23:05 -0800

    created writeJSON method and started using MarshelIndent to create JSON format

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"net/http"
 	"strconv"
@@ -8,6 +9,9 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
+type envelope map[string]interface{}
+
+// This gets the ID parameter from an http request
 func (app *application) readIDParam(r *http.Request) (int64, error) {
 	params := httprouter.ParamsFromContext(r.Context())
 
@@ -17,4 +21,25 @@ func (app *application) readIDParam(r *http.Request) (int64, error) {
 	}
 
 	return id, nil
+}
+
+// This takes the destination http.ResponseWriter, the HTTP status code to send,
+// the data to encode to JSON, and a header map containing any additional HTTP
+// headers we want to include in the response.
+func (app *application) writeJSON(w http.ResponseWriter, status int, data envelope, headers http.Header) error {
+	js, err := json.MarshalIndent(data, "", "\t")
+	if err != nil {
+		return err
+	}
+
+	js = append(js, '\n')
+
+	for key, val := range headers {
+		w.Header()[key] = val
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	w.Write(js)
+
+	return nil
 }
